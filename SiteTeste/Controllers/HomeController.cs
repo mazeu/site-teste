e1bb6ed2b605a4b2059e5b78f61e686eeb85ac7b
mazeu <marciomazeu@hotmail.com>
2023-02-19 20:26:16 -0300

    Upload files

---
+++ SiteTeste/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SiteTeste.Models;
+
+namespace SiteTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Marcio Mazeu";
+            home.Email = "marciomazeu@gmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SiteTeste/Models/HomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SiteTeste.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
