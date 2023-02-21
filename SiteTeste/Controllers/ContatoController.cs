e674ec85c86f2d399d604c6d1ba99f80102b670f
mazeu <marciomazeu@hotmail.com>
2023-02-20 23:08:46 -0300

    Entity framework installed

---
+++ SiteTeste/Controllers/ContatoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteTeste.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarContato()
+        {
+            return View();
+        }
+
+        public IActionResult EditarContato()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarContato()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SiteTeste/Data/BancoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SiteTeste.Data
+{
+    public class BancoContext :DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) :base(options)
+        {
+
+        }
+    }
+}
